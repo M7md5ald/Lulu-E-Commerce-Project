b118035ec502d3d80cffa129499430dad19aa438
M7mdishere <mohamed.khaled1640@gmail.com>
2025-08-19 17:24:59 +0300

    I set up admin dashboard page and added the assets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
