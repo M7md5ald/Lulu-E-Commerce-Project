4eda36290fab366e0565e98b68fb9c9481499004
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-08-19 15:23:32 +0300

    Frontend for admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en" data-bs-theme="light">
+
+
+<!-- Mirrored from codervent.com/matoxi/demo/vertical-menu/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 08 Sep 2024 08:59:03 GMT -->
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Metoxi | Bootstrap 5 Admin Dashboard Template</title>
+  <!--favicon-->
+	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png">
+
+  <!--plugins-->
+  <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="assets/plugins/metismenu/metisMenu.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/plugins/metismenu/mm-vertical.css">
+  <link rel="stylesheet" type="text/css" href="assets/plugins/simplebar/css/simplebar.css">
+  <!--bootstrap css-->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600&amp;display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Material+Icons+Outlined" rel="stylesheet">
+  <!--main css-->
+  <link href="assets/css/bootstrap-extended.css" rel="stylesheet">
+  <link href="sass/main.css" rel="stylesheet">
+  <link href="sass/dark-theme.css" rel="stylesheet">
+  <link href="sass/semi-dark.css" rel="stylesheet">
+  <link href="sass/bordered-theme.css" rel="stylesheet">
+  <link href="sass/responsive.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!--start header-->
+  @include('admin.header')
+  <!--end top header-->
+
+
+  <!--start sidebar-->
+  @include('admin.sidebar')
+  <!--end sidebar-->
+
+
+  <!--start main wrapper-->
+  @include('admin.main_wrapper')
+  <!--end main wrapper-->
+
+  <!--start overlay-->
+    <div class="overlay btn-toggle"></div>
+  <!--end overlay-->
+
+ <!--start footer-->
+ <footer class="page-footer">
+  <p class="mb-0">Copyright © 2023. All right reserved.</p>
+</footer>
+<!--top footer-->
+
+  <!--start cart-->
+  @include('admin.cart')
+  <!--end cart-->
+
+  <!--start switcher-->
+  @include('admin.switcher')
+  <!--start switcher-->
+
+
+  <!--bootstrap js-->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+  <!--plugins-->
+  <script src="assets/js/jquery.min.js"></script>
+  <!--plugins-->
+  <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+  <script src="assets/plugins/metismenu/metisMenu.min.js"></script>
+  <script src="assets/plugins/apexchart/apexcharts.min.js"></script>
+  <script src="assets/js/index.js"></script>
+  <script src="assets/plugins/peity/jquery.peity.min.js"></script>
+  <script>
+    $(".data-attributes span").peity("donut")
+  </script>
+  <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
+  <script src="assets/js/main.js"></script>
+
+
+</body>
+
+
+<!-- Mirrored from codervent.com/matoxi/demo/vertical-menu/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 08 Sep 2024 08:59:56 GMT -->
+</html>
