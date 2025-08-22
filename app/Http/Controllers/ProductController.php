2227afb4c8f94ffcacd975628aa9ca9b97001f26
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-08-22 15:22:42 +0300

    temp change in products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    //
+    public function createProducts(){
+        $categories = Category::all();
+        
+        return view('products.create', compact('categories'));
+    }
+
+    public function storeProducts(Request $request){
+        
+        $request->validate([
+            'name'=> 'required|string|max:255',
+            'description'=>'required|string',
+            'price'=>'required|numeric',
+            //'quantity'=>'required|integer',
+            'image'=>'required|',
+            'category_id'=>'required|string|max:255',
+        ]);
+
+        $imagePath = null;
+        if($request->hasFile('image')){
+            $imagePath = $request->file('image')->store('products', 'public');
+        }
+        
+        Product::create([
+            'name'=>$request->name,
+            'description'=>$request->description,
+            'price'=>$request->price,
+            //'quantity'=>$request->quantity,
+            'image'=>$imagePath,
+            'category_id'=>$request->category_id,
+        ]);
+        return redirect()->route('products.create')->with('success', 'Product added successfully');
+    }
+
+    public function viewProducts(){
+        $products = Product::all();
+        return view('products.view', compact('products'));
+    }
+
+    public function deleteProducts($id){
+        $products = Product::findOrFail($id);
+        $products->delete();
+        return redirect()->route('products.view')->with('success', 'Product deleted successfully');
+    }
+
+    public function editProducts($id){
+        $categories = Category::all();
+        $product = Product::findOrFail($id);
+        return view('products.edit', compact('product', 'categories'));
+    }
+
+    public function updateProducts(Request $request, $id){
+        $product = Product::findOrFail($id);
+
+        if($request->hasFile('image')){
+            if($product->image){
+                Storage::disk('public')->delete($product->image);
+            }
+            $product->image = $request->file('image')->store('products', 'public');
+        }
+
+        $product->update([
+            'name'=>$request->name,
+            'description'=>$request->description,
+            'price'=>$request->price,
+            //'quantity'=>$request->quantity,
+            'image'=>$request->image,
+            'category_id'=>$request->category_id,
+        ]);
+        return redirect()->route('products.view')->with('success', 'Product updated successfully');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\user\UserDashboardController;
 use App\Http\Controllers\admin\AdminDashboardController;
 
@@ -14,3 +15,17 @@ Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name
 
 #####    user    #####
 Route::get('/user/dashboard', [UserDashboardController::class, 'index'])->name('user.dashboard');
+
+
+####    products    ####
+Route::get('/admin/products/create', [ProductController::class, 'createProducts'])->name('products.create');
+
+Route::post('/admin/products/post', [ProductController::class, 'storeProducts'])->name('products.store');
+
+Route::get('/admin/products/view', [ProductController::class, 'viewProducts'])->name('products.view');
+
+Route::delete('/admin/products/{id}', [ProductController::class, 'deleteProducts'])->name('products.delete');
+
+Route::get('/admin/products/{id}/edit', [ProductController::class, 'editProducts'])->name('products.edit');
+
+Route::put('/admin/products/{id}', [ProductController::class, 'updateProducts'])->name('products.update');
